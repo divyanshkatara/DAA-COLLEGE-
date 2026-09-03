ead8cbebe2888e13034802f41117262378111844
Divu <divyanshkatara121@gmail.com>
2026-09-03 15:59:47 +0530

    exp(7).py

---
+++ exp(7).py
@@ -0,0 +1,28 @@
+def merge_sort(a):
+    if len(a) <= 1:
+        return a
+
+    mid = len(a) // 2
+
+    left = merge_sort(a[:mid])
+    right = merge_sort(a[mid:])
+
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+
+        else:
+            result.append(right[j])
+            j += 1
+
+    return result + left[i:] + right[j:]
+
+
+a = [5, 2, 8, 1, 3]
+
+print("Sorted:", merge_sort(a))
